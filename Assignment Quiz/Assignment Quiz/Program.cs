20485a4537dc4917f606bcbfe84ff7d215d38a3f
unknown <Admin@CSLAB246.CSSERVER.local>
2024-02-13 18:36:47 +0800

    Assignment Quiz

---
+++ Assignment Quiz/Assignment Quiz/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+
+class Person
+{
+
+    public string Name { get; set; }
+    public int Weight { get; set; }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>();
+
+            while (true)
+            {
+                Console.WriteLine("Enter a name: ");
+                string name = Console.ReadLine();
+
+                if (name.ToLower() == "exit")
+                {
+                    break;
+                }
+
+                Console.WriteLine("Enter a weight: ");
+                string weightInput = Console.ReadLine();
+                int weight;
+                if (int.TryParse(weightInput, out weight))
+                {
+                    Person person = new Person { Name = name, Weight = weight };
+                    people.Add(person);
+                    Console.WriteLine($"New added name: {name} weight: {weight}");
+                }
+                else
+                {
+                    Console.WriteLine("Warning: Invalid Input!");
+                   
+                }
+            }
+
+            Console.WriteLine("*****************************");
+            foreach (Person person in people)
+            {
+                Console.WriteLine($"{person.Name}: {person.Weight}");
+            }
+        }
+
+
+    }
+}
